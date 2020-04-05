649d3f8510222d5583e1721f937586432d291ddf
tuphi <tuphi.bka@gmail.com>
2020-04-05 14:58:57 +0700

    basic code after adding ejs

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Set up ejs
+app.set("view engine", "ejs");
+
 app.listen(3000, function(res) {
   console.log("Server is running on port 3000.");
 })
 
 app.get("/", function(req, res) {
   var today = new Date();
-  if(today.getDay() === 5 || today.getDay() === 6) {
-    res.sendFile(__dirname + "/weekend.html");
+  var day = "";
+  if(today.getDay() === 0 || today.getDay() === 6) {
+    day = "Weekend";
   } else {
-    res.sendFile(__dirname + "/weekday.html");
+    day = "Weekday";
   }
+  res.render("list", {kindOfDay: day});
 })
