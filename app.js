d9a2cd98fdd7e2d416c47ad3091f3530eac1b6a1
tuphi <tuphi.bka@gmail.com>
2020-04-06 10:42:07 +0700

    passing data between ejs and server, render with dynamic content

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
 
 // Set up ejs
 app.set("view engine", "ejs");
 
+// Global variables
+var tasks = [];
+
 app.listen(3000, function(res) {
   console.log("Server is running on port 3000.");
 })
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   var today = new Date();
@@ -19,9 +23,11 @@ app.get("/", function(req, res) {
   var options = { weekday: "long", month: "short", day: "numeric"};
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, taskItems: tasks});
 })
 
 app.post("/", function(req, res) {
-  console.log(req.body.newTask);
+  var task = req.body.newTask;
+  tasks.push(task);
+  res.redirect("/");
 })
